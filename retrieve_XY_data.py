ede6fdd5666a0e3d23dc1024d755c7b11f7ee246
Kalimero <baptiste.mamouret@gadz.org>
2023-12-07 15:48:25 +0100

    sals

--- retrieve_XY_data.py
+++ retrieve_XY_data.py
@@ -10,15 +10,11 @@ odb = abaqus.session.openOdb(name= "combinaison_48.odb")
 # Nom de la step
 nom_step = 'load'  # Remplace par le nom de ta step
 
-# ID du nœud pour lequel tu veux les RF
 nodeID = 5
 
-# Accède à la step
 step = odb.steps[nom_step]
 
-# Parcours tous les frames de la step
 for frame in step.frames:
-    # Extrait les RF pour le nœud spécifié
     try:
         rf = frame.fieldOutputs['RF'].getSubset(region=odb.rootAssembly.nodeSets['ALL NODES']).values
         for value in rf:
